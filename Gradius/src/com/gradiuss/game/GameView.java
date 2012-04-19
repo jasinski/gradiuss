be6e9ee66e6f9829c4fa58634445aad607602c7a
Michael Jasinski <michaljas88@gmail.com>
2012-04-19 14:18:36 +0200

    Added vertical movement for the spaceship

--- Gradius/src/com/gradiuss/game/GameView.java
+++ Gradius/src/com/gradiuss/game/GameView.java
@@ -9,7 +9,6 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -20,12 +19,22 @@ import com.gradiuss.game.models.TypeOneProjectile;
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 	
 	private static final String TAG = GameView.class.getSimpleName();
-	private GameLoopThread gameLoop;
+	GameLoopThread gameLoop;
+	
+	// :::::::::::::::::::::::::::::::::::::::::::::: Fields ::::::::::::::::::::::::::::::::::::::::::::::
+	
+	// Game time
+	//long totalGameTime;
 	
 	// GameObjects
+	// SpaceShip
 	public SpaceShip spaceShip;
+	// Projectiles
 	public List<Projectile> projectiles;
 	int projectileType = 0;
+	long fireTime;
+	long previousFireTime = 0;
+	// Enemies
 	
 	public GameView(Context context, AttributeSet attributes) {
 		super(context, attributes);
@@ -43,6 +52,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 		// Starting game loop
 		gameLoop.setRunning(true);
 		gameLoop.start();
+		//totalGameTime = System.currentTimeMillis();
 	}
 	
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
@@ -68,7 +78,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 		setFocusable(true);
 	}
 
-	
+	 
 	public void initGameObjects() {
 		initSpaceShip();
 		initProjectiles();
@@ -80,12 +90,15 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 		Bitmap spaceShipBitmap = BitmapFactory.decodeResource(this.getContext().getResources(), R.drawable.spaceship);
 		spaceShip = new SpaceShip(spaceShipBitmap, getWidth()/2, getHeight()-spaceShipBitmap.getHeight(), 5, 5);
 		spaceShip.setVx(10);
+		spaceShip.setVy(10);
 		spaceShip.setVisible(true);
 	}
 	
 	private void initProjectiles() {
 		// Projectiles
+		
 		projectiles = new ArrayList<Projectile>();
+		//fireTime = 1;
 	}
 	
 	private void initEnemies() {
@@ -102,18 +115,18 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 	}
 	
 	public void updateProjectiles() {
-		// Skjuter projektiler
-		if (spaceShip.isShooting()) {
+		// Shooting projectiles
+		if (spaceShip.isShooting()/* && totalGameTime - previousFireTime > fireTime*/) {
+			//previousFireTime = totalGameTime;
 			addProjectile(spaceShip.getX(), spaceShip.getY());
 		}
 		
 		for (int i = projectiles.size() - 1; i >= 0; i--) {
 			projectiles.get(i).updateState();
-			if (projectiles.get(i).isVisible() == false) {
+			if (!projectiles.get(i).isVisible()) {
 				projectiles.remove(i);
 			}
 		}
-		// Skjuter projektiler
 	}
 	
 	private void updateSpaceShip() {
@@ -121,8 +134,14 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 		if (spaceShip.getX() + spaceShip.getBitmap().getWidth()/2 >= getWidth()) {
 			spaceShip.setMoveRight(false);
 		}
-		if ((spaceShip.getX() - spaceShip.getBitmap().getWidth()/2 <= 0)) {
+		if (spaceShip.getX() - spaceShip.getBitmap().getWidth()/2 <= 0) {
 			spaceShip.setMoveLeft(false);
+		}
+		if (spaceShip.getY() - spaceShip.getBitmap().getHeight()/2 <= 0) {
+			spaceShip.setMoveUp(false);
+		}
+		if (spaceShip.getY() + spaceShip.getBitmap().getHeight()/2 >= getHeight()) {
+			spaceShip.setMoveDown(false);
 		}
 		spaceShip.updateState();
 	}
@@ -166,111 +185,6 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 		for (int i = 0; i < projectiles.size(); i++) {
 			projectiles.get(i).draw(canvas);
 		}
-	}
-	
-	// :::::::::::::::::::::::::::::::::::::::::::::: GameLoopThread ::::::::::::::::::::::::::::::::::::::::::::::
-
-	public class GameLoopThread extends Thread {
-		private final String TAG = GameLoopThread.class.getSimpleName();
-		private boolean running;
-		private SurfaceHolder surfaceHolder;
-		private GameView gameView;
-		
-		// Frames per second
-		private final static int MAX_FPS = 50;
-		// Max amount of frames skipped
-		private final static int MAX_FRAMES_SKIPPED = 5;
-		// Length of the period
-		private final static int UPDATE_RENDER_PERIOD = 1000 / MAX_FPS;
-		
-		public GameLoopThread(SurfaceHolder surfaceHolder, GameView gameView) {
-			this.surfaceHolder = surfaceHolder;
-			this.gameView = gameView;
-		}
-		
-		@Override
-		public void run() {
-			
-			Canvas canvas;
-			
-			// FPS
-			long startTime;
-			long endTime;
-			long timeDifference;
-			int sleepTime;
-			int skippedFrames;
-			
-			Log.d(TAG, "Starting game loop");
-			while (running) {
-				canvas = null;
-				try {
-					
-					// Locking canvas so that objects can draw themselves on it
-					canvas = this.surfaceHolder.lockCanvas();
-					
-					synchronized (surfaceHolder) {
-						
-						// Start timer
-						startTime = System.currentTimeMillis();
-						
-						// Update the game
-						updateGameState();
-						
-						// Render the game
-						renderGameState(canvas);
-						
-						// Stop timer and measure the timedifference as well as how long we shall sleep
-						endTime = System.currentTimeMillis();
-						timeDifference = startTime - endTime;
-						sleepTime = (int) (UPDATE_RENDER_PERIOD - timeDifference);
-						
-						// If sleepTime is larger than 0 then sleep
-						if (sleepTime > 0) {
-							try {
-								sleep(sleepTime);
-							} catch (InterruptedException e) {
-								e.printStackTrace();
-							}
-						}
-						
-						// If sleepTime is smaller than 0 we need to catch up
-						skippedFrames = 0;
-						while (sleepTime < 0 && skippedFrames < MAX_FRAMES_SKIPPED) {
-							updateGameState();
-							sleepTime = sleepTime + UPDATE_RENDER_PERIOD;
-							skippedFrames++;
-						}
-						
-					}
-				} finally {
-					if (canvas != null) {
-						
-						// Unlocking canvas and displaying it
-						surfaceHolder.unlockCanvasAndPost(canvas);
-					}
-				}
-				
-			}
-		}
-		
-		/**
-		 * Wrapper for the actual update method in the game view.
-		 */
-		public void updateGameState() {
-			gameView.updateState();
-		}
-		
-		/**
-		 * Wrapper for the actual rendering method in the game view.
-		 */
-		public void renderGameState(Canvas canvas) {
-			gameView.renderState(canvas);
-		}
-		
-		public void setRunning(boolean running) {
-			this.running = running;
-		}
-
 	}
 	
 }
--- Gradius/src/com/gradiuss/game/GameViewActivity.java
+++ Gradius/src/com/gradiuss/game/GameViewActivity.java
@@ -18,6 +18,8 @@ public class GameViewActivity extends Activity {
 	GameView gameView;
 	Button bLeftPad; // Move spaceship left
 	Button bRightPad; // Move spaceship right
+	Button bUpPad; // Move spaceship up
+	Button bDownPad; // Move spaceship down
 	Button bFire; // Fire projectiles
 	
 	@Override
@@ -35,6 +37,8 @@ public class GameViewActivity extends Activity {
         gameView = (GameView) findViewById(R.id.gameView);
         bLeftPad = (Button) findViewById(R.id.bLeftPad);
         bRightPad = (Button) findViewById(R.id.bRightPad);
+        bUpPad = (Button) findViewById(R.id.bUpPad);
+        bDownPad = (Button) findViewById(R.id.bDownPad);
         bFire = (Button) findViewById(R.id.bFire);
         
         // Setting onTouch listeners
@@ -76,6 +80,44 @@ public class GameViewActivity extends Activity {
 			}
 		});
         
+        bUpPad.setOnTouchListener(new OnTouchListener() {
+			
+			public boolean onTouch(View v, MotionEvent event) {
+				
+				// Move spaceship up
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					bUpPad.setPressed(true);
+					gameView.spaceShip.setMoveUp(true);
+				}
+				
+				// Stop moving spaceship up
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					gameView.spaceShip.setMoveUp(false);
+					bUpPad.setPressed(false);
+				}
+				return true;
+			}
+		});
+        
+        bDownPad.setOnTouchListener(new OnTouchListener() {
+			
+			public boolean onTouch(View v, MotionEvent event) {
+				
+				// Move spaceship down
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					bDownPad.setPressed(true);
+					gameView.spaceShip.setMoveDown(true);
+				}
+				
+				// Stop moving spaceship down
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					gameView.spaceShip.setMoveDown(false);
+					bDownPad.setPressed(false);
+				}
+				return true;
+			}
+		});
+        
         bFire.setOnTouchListener(new OnTouchListener() {
 			
 			public boolean onTouch(View v, MotionEvent event) {
@@ -96,6 +138,20 @@ public class GameViewActivity extends Activity {
 		});
         
     }
+	
+	@Override
+	protected void onPause() {
+		Log.d(TAG, "Pausing...");
+		super.onPause();
+		gameView.gameLoop.pauseThread();
+	}
+	
+	@Override
+	protected void onResume() {
+		Log.d(TAG, "Resuming...");
+		super.onResume();
+		gameView.gameLoop.resumeThread();
+	}
 
 	@Override
 	protected void onDestroy() {
