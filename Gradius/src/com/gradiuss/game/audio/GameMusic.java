7d0589f46eb4da60d203e12e33032605dec25f87
Michael Jasinski <michaljas88@gmail.com>
2012-05-09 00:09:44 +0200

    Refactoring of the code to work with future update concerning new levels in the game. The GameView class now contains a list of levels and a pointer that will traverse the list when a new level is reached. So far there is only one level, but the code is now ready to handle more than this. Also added som update code for sound effects and music, see new classes. These are not finished yet though.

---
+++ Gradius/src/com/gradiuss/game/audio/GameMusic.java
@@ -0,0 +1,42 @@
+package com.gradiuss.game.audio;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+public class GameMusic {
+	
+	private static MediaPlayer mediaPlayer;
+	private static Context musicContext;
+	
+	public static void setContext(Context context) {
+		musicContext = context;
+	}
+	
+	public static void setMusic(int resId) {
+		if (mediaPlayer != null) {
+			mediaPlayer.reset(); 
+		}
+		mediaPlayer = MediaPlayer.create(musicContext, resId);
+	}
+	
+	public static void playMusic() {
+		mediaPlayer.start();
+	}
+	
+	public static void loopMusic(boolean loop) {
+		mediaPlayer.setLooping(loop);
+	}
+	
+	public static void pauseMusic() {
+		mediaPlayer.pause();
+	}
+	
+	public static void stopMusic() {
+		mediaPlayer.stop();
+	}
+	
+	public static void setVolume(float volume) {
+		mediaPlayer.setVolume(volume, volume);
+	}
+
+}
