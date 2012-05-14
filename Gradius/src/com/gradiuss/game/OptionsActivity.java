8a1f57ffc113feebc1e0b5570e601df7d65aa4da
martinbackman <martinbackman1@gmail.com>
2012-05-14 09:52:04 +0200

    getting latest version

---
+++ Gradius/src/com/gradiuss/game/OptionsActivity.java
@@ -0,0 +1,16 @@
+package com.gradiuss.game;
+
+import com.gradiuss.game.highscore.SQLViewActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OptionsActivity extends Activity{
+	private static final String TAG = HelpActivity.class.getSimpleName();
+	
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.options);
+		
+	}	
+}
