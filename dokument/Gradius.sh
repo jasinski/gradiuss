2069ef751f1af2c3ba4b0f9ff394039eb85c8c42
patriques82 <patriknygren82@gmail.com>
2012-05-18 14:38:14 +0200

    adding Gradius.sh script

---
+++ dokument/Gradius.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Requires git, ant and that the tools and platform-tools 
+# folder in the Android SDK is on your path.
+#
+# Retrieve the latest version from your repository﻿
+#
+git clone git@github.com:jasinski/gradiuss.git
+#
+# Generate build.xml
+#
+cd gradiuss/
+android update project --path .
+cd ..
+android update test-project -m ../Gradius -p TestGradius/
+#
+# Compile, sign with debug key install to emulator/device and
+# run tests.
+#
+cd TestGradius
+ant run-tests
